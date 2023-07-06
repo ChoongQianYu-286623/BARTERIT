501e3aeada3fb095f2b50306a5a17cddf513152b
Choong Qian Yu <92418780+ChoongQianYu-286623@users.noreply.github.com>
2023-07-06 19:12:36 +0800

    Update load_item.php

--- php/load_item.php
+++ php/load_item.php
@@ -7,15 +7,30 @@ if (!isset($_POST)) {
 
 include_once("dbconnect.php");
 
+$results_per_page = 8;
+if (isset($_POST['pageno'])){
+	$pageno = (int)$_POST['pageno'];
+}else{
+	$pageno = 1;
+}
+$page_first_result = ($pageno - 1) * $results_per_page;
+
 if (isset($_POST['userid'])){
 	$userid = $_POST['userid'];	
 	$sqlloaditem = "SELECT * FROM `tbl_items` WHERE  user_id = '$userid'";
-}
-else{
+}else if (isset($_POST['search'])){
+	$search = $_POST['search'];
+	$sqlloaditem = "SELECT * FROM `tbl_items` WHERE item_name LIKE '%$search%'";
+}else{
 	$sqlloaditem = "SELECT * FROM `tbl_items`";
 }
 
 $result = $conn->query($sqlloaditem);
+$number_of_result = $result->num_rows;
+$number_of_page = ceil($number_of_result / $results_per_page);
+$sqlloaditem = $sqlloaditem . " LIMIT $page_first_result , $results_per_page";
+$result = $conn->query($sqlloaditem);
+
 if ($result->num_rows > 0) {
     $item["item"] = array();
 	while ($row = $result->fetch_assoc()) {
@@ -34,7 +49,7 @@ if ($result->num_rows > 0) {
 		$itemlist['insert_date'] = $row['insert_date'];
         array_push($item["item"],$itemlist);
     }
-    $response = array('status' => 'success', 'data' => $item);
+	$response = array('status' => 'success', 'data' => $item, 'numofpage'=>"$number_of_page",'numberofresult'=>"$number_of_result");
     sendJsonResponse($response);
 }else{
      $response = array('status' => 'failed', 'data' => null);
